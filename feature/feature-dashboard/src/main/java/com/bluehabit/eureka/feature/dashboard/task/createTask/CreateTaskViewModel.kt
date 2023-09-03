1adcc7d6a8e8397eab45b59dd7588600bc8d383e
fauzangraciaramadhani <fauzanramadhani06@gmail.com>
2023-09-03 14:07:44 +0700

    feat: initiate task package structure

---
+++ feature/feature-dashboard/src/main/java/com/bluehabit/eureka/feature/dashboard/task/createTask/CreateTaskViewModel.kt
@@ -0,0 +1,23 @@
+/*
+ * Copyright © 2023 Blue Habit.
+ *
+ * Unauthorized copying, publishing of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
+package com.bluehabit.eureka.feature.dashboard.task.createTask
+
+import app.trian.mvi.ui.viewModel.MviViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CreateTaskViewModel @Inject constructor(
+):MviViewModel<CreateTaskState,CreateTaskAction>(
+    CreateTaskState()
+) {
+    override fun onAction(action: CreateTaskAction) {
+        TODO("Not yet implemented")
+    }
+
+}
